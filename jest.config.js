0a45e30ac7603741105c0888b0cae9015fde3d91
Mateusz Wit <mateusz.wit@travelport.com>
2019-03-20 16:28:14 +0100

    Show coverage for all source code files when testing with jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleFileExtensions: ['js', 'ts', 'tsx'],
-  "snapshotSerializers": ["enzyme-to-json/serializer"],
+  preset: "ts-jest",
+  testEnvironment: "node",
+  collectCoverageFrom : ["src/**/*.tsx"],
+  moduleFileExtensions: ["js", "ts", "tsx"],
+  snapshotSerializers: ["enzyme-to-json/serializer"],
   setupFilesAfterEnv: ["<rootDir>enzyme.config.js"],
   globals: {
     "ts-jest": {
